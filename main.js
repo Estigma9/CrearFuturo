3f51dbf6a7b3539d10806e1fd7918bc3d68e1195
Estigma9 <esduro@gmail.com>
2023-11-04 09:49:19 -0500

    CrearFuturto

---
+++ main.js
@@ -0,0 +1,27 @@
+/**/
+// Mi comentario de linea
+
+/*Mi comentario
+de  muchas
+lineas*/
+
+//Calculo del Perimetro de un Cuadrado
+
+let lado = 5;
+
+let perimetro = 4*lado;
+let area = lado*lado;
+
+document.getElementById('algoritmo').innerHTML = "El perimetro es: " + perimetro + " y el área es: " + area;
+
+let lado1 = 5;
+let lado2 = 6;
+
+let perimetroRectangulo = 2*lado2 + 2*lado2;
+let areaRectangulo = lado1*lado2;
+
+document.getElementById('rectangulo').innerHTML = "El perimetro del rectangulo es: " + perimetroRectangulo + " y el área del rectangulo es: " + areaRectangulo;
+
+
+
+
